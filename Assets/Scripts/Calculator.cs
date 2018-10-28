5b7e72ca435029905d28a298d33fcc05accdbb78
RiloJino <Takeozabi@gmail.com>
2018-10-27 19:58:05 -0700

    Display Frame Work
    created canvas with buttons for input and a display of what has already
    been entered

---
+++ Assets/Scripts/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Calculator : MonoBehaviour {
+
+	[SerializeField]
+	Text inputText;
+	string buttonValue;
+
+	public void ButtonPressed()
+    {
+		buttonValue = EventSystem.current.currentSelectedGameObject.name;
+		inputText.text = buttonValue;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
